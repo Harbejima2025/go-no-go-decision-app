9e97589e46772f543bca0d8e4970a56316dac408
Harbejima2025 <ayumideksajima@gmail.com>
2025-08-04 07:49:20 +0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,65 @@
+const questionsData = [
+  {
+    area: "Donor",
+    categories: [
+      {
+        subArea: "Donor relationship",
+        questions: [
+          {
+            id: "1.1.1",
+            grants: "Is the FO well positioned with the donor in-country, for example recent visits to the relevant technical officers of the donor?",
+            contracts: "Is the FO well positioned with the donor in-country, for example recent visits to the relevant technical officers of the donor?"
+          },
+          {
+            id: "1.1.2",
+            grants: "Has the FO had a LRF/Locally led grant from this donor before?",
+            contracts: "Is the FO familiar with the bidding process of the client, or has previous experience in applying, receiving funds, implementing project with the client?"
+          }
+        ]
+      },
+      {
+        subArea: "GESI policy",
+        questions: [
+          {
+            id: "1.2.1",
+            grants: "Is FO team familiar with donor's Gender Equality and Social Inclusion (GESI) strategy/policy/minimum requirements?",
+            contracts: "Is FO team familiar with donor's Gender Equality and Social Inclusion (GESI) strategy/policy/minimum requirements?"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    area: "Partners",
+    categories: [
+      {
+        subArea: "Implementing Partners",
+        questions: [
+          {
+            id: "2.1.1",
+            grants: "Does the FO have or can it quickly establish competitive implementing partners/consortia needed to succeed? Who?",
+            contracts: "Does the FO have or can it quickly establish competitive implementing partners/consortia needed to succeed?  In the comments indicate the names"
+          },
+          {
+            id: "2.1.2",
+            grants: "Does the FO have partnering/consortia/teaming agreements in place for this opportunity? Did/will partners pass due diligence checks?",
+            contracts: "Does the FO have partnering/consortia/teaming agreements in place for this opportunity? Did/will partners pass due diligence checks?"
+          }
+          // ... more under Implementing Partners
+        ]
+      },
+      {
+        subArea: "Competitors",
+        questions: [
+          {
+            id: "2.2.1",
+            grants: "Are there strong competitors to winning this grant? Who? How do we rate compared to the competition?",
+            contracts: "Are there strong competitors to winning this grant? Who? How do we rate compared to the competition?"
+          }
+        ]
+      }
+      // ... other subAreas like Partnering Requirement, Bidding Alone, etc.
+    ]
+  }
+  // ... More areas like Alignment, Proposal Writing, etc.
+];
